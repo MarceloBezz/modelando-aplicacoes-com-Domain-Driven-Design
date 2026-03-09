a7cd26df1cc8966360fd1939730bf3ef75c39a19
Marcelo <mdbezerra16@gmail.com>
2026-03-09 11:32:54 -0300

    refactor: :recycle: Organização das classes de domínio
    Criação da pasta "dominio" e rganização interna de "evento" e "ingresso"

---
+++ src/main/java/br/com/alura/codechella/dominio/evento/Endereco.java
@@ -0,0 +1,20 @@
+package br.com.alura.codechella.dominio.evento;
+
+public class Endereco {
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+}
---
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -0,0 +1,45 @@
+package br.com.alura.codechella.dominio.evento;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.codechella.Categoria;
+import br.com.alura.codechella.dominio.ingresso.TipoIngresso;
+
+public class Evento {
+    private Categoria categoria;
+    private String descricao;
+    private Endereco endereco;
+    private LocalDateTime data;
+    private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso) {
+        tipoIngressos.add(tipoIngresso);
+    }
+    
+    public void aumentaNumeroDeIngressosPorTipo(TipoIngresso tipoIngresso, int quantidade) {
+        // Implementar aqui
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public List<TipoIngresso> getTipoIngressos() {
+        return tipoIngressos;
+    }
+
+}
