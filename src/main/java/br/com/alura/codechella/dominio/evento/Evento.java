9ba6558fa841e4a89221e307644d09c3412490a4
Marcelo <mdbezerra16@gmail.com>
2026-03-09 13:59:40 -0300

    feat: :sparkles: Atributo uuid em Usuario
    Exemplo de interfaces criadas na aplicacao que seriam implementadas por outras classes na camada de infra

--- src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -3,15 +3,30 @@ package br.com.alura.codechella.dominio.evento;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import br.com.alura.codechella.dominio.ingresso.TipoIngresso;
 
 public class Evento {
+    private UUID uuid;
     private Categoria categoria;
     private String descricao;
     private Endereco endereco;
     private LocalDateTime data;
     private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public Evento(Categoria categoria, String descricao, Endereco endereco, LocalDateTime data) {
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.endereco = endereco;
+        this.data = data;
+
+        gerarIdentificadorUnico();
+    }
+
+    private void gerarIdentificadorUnico() {
+        this.uuid = UUID.randomUUID();
+    }
 
     public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso) {
         tipoIngressos.add(tipoIngresso);
@@ -39,6 +54,10 @@ public class Evento {
 
     public List<TipoIngresso> getTipoIngressos() {
         return tipoIngressos;
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 
 }
